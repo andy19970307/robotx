84e11389f9287ec5929e36e3ba1755f54e254764
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-11-30 21:30:52 +0800

    object detect

--- catkin_ws/src/detect_object/src/detect_object.py
+++ catkin_ws/src/detect_object/src/detect_object.py
@@ -3,10 +3,8 @@ import rospy
 import numpy as np
 import math
 import argparse
-from duckietown_msgs.msg import  Twist2DStamped
 from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge, CvBridgeError
-from duckietown_utils.jpg import image_cv_from_jpg
 from matplotlib import pyplot as plt
 import cv2
 import sys
@@ -18,31 +16,13 @@ import imutils
 class andy(object):
 	def __init__(self):
 		self.node_name = rospy.get_name()
-
 		self.thread_lock = threading.Lock()
 		self.active = True
-
-		# to do: initial no-faces-detected as face_detected senario
-		self.face_detected = 0
-
 		self.bridge = CvBridge()
 
 		# Publicaiton
 
-		# To do : publish ros message topic: /node_name/car_cmd, datatype: Twist2DStamped 
-		#plt.rcParams['figure.figsize'] = (10, 10)
-		#plt.rcParams['image.interpolation'] = 'nearest'
-
-		# To do : publish ros message topic: /node_name/image_with_face, datatype: Image 
-		self.pub_image_face = rospy.Publisher("~image_with_face", Image, queue_size=1)
-		self.pub_image_HSV = rospy.Publisher("~image_with_HSV", Image, queue_size=1)
-		self.pub_image_edge = rospy.Publisher("~image_with_edge", Image, queue_size=1)
-
         # Subscription
-
-		# To do : subscribe ros message topic: /node_name/joystick_car_cmd datatype: Twist2DStamped, callback function: self.cbJoystick
-
-		# To do : subscribe ros message topic: /node_name/image, datatype: CompressedImage, callback function: self.cbImage
 		self.sub_image_origin = rospy.Subscriber("andyduck/camera_node/image/compressed", CompressedImage, self.cbImage, queue_size=10)
 
 		# safe shutdown
@@ -70,7 +50,6 @@ class andy(object):
 	def processImage(self, image_msg):
 		if not self.thread_lock.acquire(False):
 			return
-
 		try:
 			self.cbObjectDetect(image_msg)
 		finally:
@@ -104,17 +83,10 @@ class andy(object):
 		# Decompress image and convert ROS image message to cv image
 		narr = np.fromstring(image_msg.data, np.uint8)
 		image = cv2.imdecode(narr, cv2.IMREAD_COLOR)
-		#image = image_cv_from_jpg(image_msg.data)
+
 		cir = [0,0,0]
 		a=[0,0]
 		box = [a,a,a,a]
-
-		edges = cv2.Canny(image,100,200)
-		hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-		lowerblue = np.array([110,50,50])
-		upperblue = np.array([130,255,255])
-		mask2 = cv2.inRange(hsv,lowerblue,upperblue)
-		mask = cv2.Canny(mask2,100,200)
 
 		MAXAREA = 18000
 		MINAREA = 50
@@ -147,7 +119,6 @@ class andy(object):
 					c = c.astype("int")
 					if shape is "square" or shape is "triangle":
 						rect = cv2.minAreaRect(c)
-					#rect = ((rect[0][0] * self.scale_down, rect[0][1] * self.scale_down), (rect[1][0] * self.scale_down, rect[1][1] * self.scale_down), rect[2])
 						box = cv2.boxPoints(rect)
 						box = np.int0(box)
 						if self.check_square(box):
@@ -155,32 +126,22 @@ class andy(object):
 							cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 							print box
 							cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255), 2)
-    
 		image_msg_out = self.bridge.cv2_to_imgmsg(image, "bgr8")
-		#image_msg_out_hsv = self.bridge.cv2_to_imgmsg(mask2, "8UC1")
 		image_msg_out_edge = self.bridge.cv2_to_imgmsg(gray)
 		image_msg_out.header.stamp = image_msg.header.stamp
-
-
-		self.pub_image_face.publish(image_msg_out)
-		#self.pub_image_HSV.publish(image_msg_out_hsv)
-		#self.pub_image_edge.publish(image_msg_out_edge)
  
 class ShapeDetector:
 	def __init__(self):
 		pass
- 
 	def detect(self, c):
 		# initialize the shape name and approximate the contour
 		shape = "unidentified"
 		peri = cv2.arcLength(c, True)
 		c1 = cv2.convexHull(c)
 		approx = cv2.approxPolyDP(c1, 0.04 * peri, True)
-
 		# if the shape is a triangle, it will have 3 vertices
 		if len(approx) == 3:
 			shape = "triangle"
- 
 		# if the shape has 4 vertices, it is either a square or
 		# a rectangle
 		elif len(approx) == 4:
@@ -188,15 +149,10 @@ class ShapeDetector:
 			# bounding box to compute the aspect ratio
 			(x, y, w, h) = cv2.boundingRect(approx)
 			ar = w / float(h)
- 
 			shape = "square" if ar >= 0.7 and ar <= 1.3 else "rectangle"
  			if h < 5 or w < 5:
  				shape = "unidentified" 
-
 if __name__ == "__main__":
-
-	# to do: initial a node named "face_detector_X", X= you duckiebot name
 	rospy.init_node("face_detector",anonymous=False)
-
 	face_detector_wama_node = andy()
 	rospy.spin()
